845139eb986b9c7ec41bb62b02993b0750134314
PeterTheOne <petertheone@gmail.com>
2014-10-07 20:14:31 +0200

    init

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>tugraz - Studies overview</title>
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="container">
+            <article>
+                <h1>tugraz - Studies overview</h1>
+                <p>
+                    This is an overview of my study of
+                    <a href="http://portal.tugraz.at/portal/page/portal/TU_Graz/Studium_Lehre/Studien/Softwareentwicklung_mag" hreflang="de">Software Development and Business Management</a>
+                    at the <a href="http://tugraz.at" hreflang="de">Graz University of Technology</a>, inspired by <a href="http://lukas-prokop.at/tug/">lukas-prokop.at/tug/</a>.
+                </p>
+
+                <h2>Blackboard</h2>
+                <dl>
+                    <dt>Last update</dt>
+                    <dd>07.10.2014</dd>
+
+                    <dt>Curriculum</dt>
+                    <dd><a href="http://mibla.tugraz.at/12_13/Stk_16f/524_MA_SWE_Wi_2013.pdf">Software Development and Business Management</a></dd>
+
+                </dl>
+
+                <h2>Progress</h2>
+                <p>
+                    todo
+                </p>
+
+                <h2>Public Content</h2>
+                <p>
+                    todo
+                </p>
+
+                <h2>Exercises </h2>
+                <p>
+                    todo
+                </p>
+
+                <h2>Exam dates</h2>
+                <p>
+                    todo
+                </p>
+
+            </article>
+
+            <footer>
+                <h2>Footer</h2>
+                <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a> Peter Grassberger. <a href="http://petergrassberger.com/contact">imprint.</a>
+            </footer>
+
+            <!-- Piwik -->
+            <script type="text/javascript">
+                var _paq = _paq || [];
+                _paq.push(["trackPageView"]);
+                _paq.push(["enableLinkTracking"]);
+
+                (function() {
+                    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.petergrassberger.com/";
+                    _paq.push(["setTrackerUrl", u+"piwik.php"]);
+                    _paq.push(["setSiteId", "1"]);
+                    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+                    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+                })();
+            </script>
+            <!-- End Piwik Code -->
+        </div>
+    </body>
+</html>
