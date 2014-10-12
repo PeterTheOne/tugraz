c6dd0a25a517091ab8d98a6bfce92b5cb1a1023f
PeterTheOne <petertheone@gmail.com>
2014-10-12 22:24:20 +0200

    add timeline

--- index.php
+++ index.php
@@ -30,6 +30,59 @@
 
                 </dl>
 
+                <h2>Timeline</h2>
+
+                <h3>October 2014</h3>
+                <ul>
+                    <li>14.10.2014: 705.061 Systemnahe Programmierung, 1. Abgabe</li>
+                    <li>21.10.2014: 705.061 Systemnahe Programmierung, 2. Abgabe</li>
+                    <li>21.10.2014?: 716.032 Entwurf und Analyse von Algorithmen, 1. Abgabe</li>
+                    <li>28.10.2014: 705.061 Systemnahe Programmierung, 3. Abgabe</li>
+                    <li>28.10.2014?: 716.032 Entwurf und Analyse von Algorithmen, 2. Abgabe</li>
+                </ul>
+
+                <h3>November 2014</h3>
+                <ul>
+                    <li>04.11.2014: 705.061 Systemnahe Programmierung, 4. Abgabe</li>
+                    <li>04.11.2014?: 716.032 Entwurf und Analyse von Algorithmen, 3. Abgabe</li>
+                    <li>05.11.2014: 705.025 Sicherheitsaspekte in der Softwareentwicklung, 1. Abgabe</li>
+                    <li>06/07.11.2014: 705.025 Sicherheitsaspekte in der Softwareentwicklung, 1. mündliche Prüfung</li>
+                    <li>11.11.2014?: 716.032 Entwurf und Analyse von Algorithmen, 4. Abgabe</li>
+                    <li>11.11.2014: 705.061 Systemnahe Programmierung, 5. Abgabe</li>
+                    <li>18.11.2014?: 716.032 Entwurf und Analyse von Algorithmen, 5. Abgabe</li>
+                    <li>18.11.2014: 705.061 Systemnahe Programmierung, 6. Abgabe</li>
+                    <li>25.11.2014: 501.072 Numerisches Rechnen und lineare Algebra, 1. Test, nicht angemeldet</li>
+                    <li>25.11.2014?: 716.032 Entwurf und Analyse von Algorithmen, 6. Abgabe</li>
+                </ul>
+
+                <h3>December 2014</h3>
+                <ul>
+                    <li>02.12.2014?: 716.032 Entwurf und Analyse von Algorithmen, 7. Abgabe</li>
+                    <li>02.12.2014: 705.061 Systemnahe Programmierung, 7. Abgabe</li>
+                    <li>03.12.2014: 705.025 Sicherheitsaspekte in der Softwareentwicklung, 2. Abgabe</li>
+                    <li>04/05.12.2014: 705.025 Sicherheitsaspekte in der Softwareentwicklung, 2. mündliche Prüfung</li>
+                    <li>09.12.2014?: 716.032 Entwurf und Analyse von Algorithmen, 8. Abgabe</li>
+                    <li>09.12.2014: 705.061 Systemnahe Programmierung, 8. Abgabe</li>
+                    <li>16.12.2014?: 716.032 Entwurf und Analyse von Algorithmen, 9. Abgabe</li>
+                    <li>16.12.2014: 705.061 Systemnahe Programmierung, 9. Abgabe</li>
+                </ul>
+
+                <h3>January 2015</h3>
+                <ul>
+                    <li>08.01.2015: 705.025 Sicherheitsaspekte in der Softwareentwicklung, 3. Abgabe</li>
+                    <li>18.01.2015: 705.025 Sicherheitsaspekte in der Softwareentwicklung, 4. Abgabe</li>
+                    <li>20.01.2015: 501.072 Numerisches Rechnen und lineare Algebra, 2. Test, nicht angemeldet</li>
+                    <li>(21-23).01.2015: 705.025 Sicherheitsaspekte in der Softwareentwicklung, 3. mündliche Prüfung</li>
+                    <li>30.01.2015: 705.024 Sicherheitsaspekte in der Softwareentwicklung, Prüfung, nicht angemeldet</li>
+                    <li>29.01.2015: 707.003 Knowledge Discovery and Data Mining 1, Prüfung</li>
+                </ul>
+
+                <h3>February 2015</h3>
+                <ul>
+                    <li>02.02.2015: 373.322 Buchhaltung und Bilanzierung, Prüfung, nicht angemeldet</li>
+                </ul>
+
+
                 <h2>Progress</h2>
                 <p>
                     todo
